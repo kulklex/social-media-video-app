da9b75d350e49bd798d5aca3408ddcb2db3edf36
kulklex <kulklex@gmail.com>
2023-02-27 16:47:35 +0100

    created VideoCard component and Impl

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,30 @@
+import NoResults from '@/components/NoResults'
+import VideoCard from '@/components/VideoCard'
+import { Video } from '@/types'
+import { BASE_URL } from '@/utils'
+import axios from 'axios'
 
-export default function Home() {
+
+
+export default function Home({videos}: {videos: Video[]}) {
+  
   return (
-    <div className=''>index</div>
+    <div className='flex flex-col gap-10 videos h-full'>{
+      videos?.length ? videos?.map((video: Video) => (<VideoCard key={video._id} post={video}/>))
+      : (<NoResults text="No Videos" />)
+    }</div>
   )
 }
+
+
+
+
+export const getServerSideProps = async () => {
+  const response = await axios.get(`${BASE_URL}/api/posts`)
+  const {data} = response
+  return {
+    props: {
+      videos: data
+    }
+  }
+} 
