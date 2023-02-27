e8dd3560717ab5e15a55e969444e97de5cdbeacf
kulklex <kulklex@gmail.com>
2023-02-27 16:46:58 +0100

    NoResults Component

---
+++ components/NoResults.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { MdOutlineVideocamOff } from 'react-icons/md';
+
+
+
+const NoResults = ({text}: { text : string}) => {
+  return (
+    <div className='flex flex-col justify-center items-center h-full w-full'>
+      <p className='text-8xl'>
+        <MdOutlineVideocamOff />
+      </p>
+      <p className='text-2xl text-center'>{text}</p>
+    </div>
+  );
+};
+
+export default NoResults
