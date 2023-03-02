78cb3892a9f55efddaaf590b145c2417378fc838
kulklex <kulklex@gmail.com>
2023-03-03 00:09:58 +0100

    Implemented Likes func

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect, useRef} from 'react'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
-import Link from 'next/link'
 import { GoVerified } from 'react-icons/go'
 import { MdOutlineCancel } from 'react-icons/md'
 import { BsFillPlayFill } from 'react-icons/bs';
@@ -9,22 +8,29 @@ import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
 import axios from 'axios'
 import { BASE_URL } from '@/utils'
 import { Video } from '../../types';
+import useAuthStore from '@/store/authStore'
+import LikeButton from '../../components/LikeButton'
+
 
 
 interface Props{
   postDetails: Video,
 }
 
+
+
 export default function PostDetail({postDetails} : Props) {
   const router = useRouter()
-  
+  const {user} : {user: any} = useAuthStore()
+
+
   const [post, setPost] = useState<Video>(postDetails) // So we can manually change the post eg when someone likes the post it'll change that post to one with a like 
   
   const videoRef = useRef<HTMLVideoElement>(null)
   const [isPlaying, setIsPlaying] = useState<boolean>(false)
   const [isVideoMuted, setIsVideoMuted] = useState<boolean>(false)
  
-
+  // Pause and Play Func
   const onVideoClick = () => {
     if(isPlaying){
       videoRef?.current?.pause()
@@ -35,18 +41,34 @@ export default function PostDetail({postDetails} : Props) {
     }
   }
 
+  // Mutes The Video
   useEffect(() => {
-    if (videoRef?.current) {
+    if (post && videoRef?.current) {
       videoRef.current.muted = isVideoMuted
     }
-   }, [isVideoMuted])
+   }, [post, isVideoMuted])
+
+
+   // Like Functions
+  const handleLike = async(like:boolean) => {
+    if(user) {
+        const {data} = await axios.put(`${BASE_URL}/api/like`, {
+          userId: user?._id,
+          postId: post?._id,
+          like: like
+        })
+
+        setPost({...post, likes: data.likes})
+    }
+  }  
   
 
+  // If post doesn't exist
   if (!post) return null
 
-  return (
-    <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
-      <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img">
+  return (<> {post && (
+    <div className='flex w-full absolute left-0 lg:left-2 top-0 lg:top-auto m-2 overflow-hidden bg-white flex-wrap lg:flex-nowrap'>
+      <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black">
         <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
           <p className='cursor-pointer' onClick={() => router.back()}>
             <MdOutlineCancel className='text-white text-[35px]'/>
@@ -54,7 +76,7 @@ export default function PostDetail({postDetails} : Props) {
         </div>
         
         <div className='relative'>
-          <div className="lg:h-[100vh] h-[60vh]">
+          <div className="lg:h-[80vh] h-[60vh]">
             <video src={post?.video?.asset?.url} className='h-full cursor-pointer'
              ref={videoRef} loop onClick={onVideoClick}></video>
           </div>
@@ -71,14 +93,34 @@ export default function PostDetail({postDetails} : Props) {
               }
         </div>
       </div>  
+
+      <div className='relative w-[1000px] md:w-[900px] lg:w-[700px]'>
+        <div className='lg:mt-20 mt-10'>
+        <div className='flex gap-4 mb-4 bg-white w-full pl-10 cursor-pointer'>
+          <Image width={62} height={62} className="rounded-full" src={post?.postedBy?.image} alt="profile photo" />
+          <div>
+            <div className='text-md text-gray-600 italic lowercase tracking-wider flex gap-2 items-center justify-center'>
+              {post?.postedBy?.userName}{' '}
+              <GoVerified className='text-blue-400 text-xl' />
+            </div>            
+            <div className='px-10 pt-3'>
+              <p className='text-xl font-bold'>{post?.caption}</p>
+            </div>
+            <div className='lg:mt-5 mt-2 px-10'>
+              {user && (<LikeButton likes={post?.likes} handleLike={() => handleLike(true)} handleDislike={() => handleLike(false)} />)}
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
-  )
+)}
+</>)
 }
 
 
 export const getServerSideProps = async ({params: {id}} : {params:{id: string}}) => {
     const {data} = await axios.get(`${BASE_URL}/api/posts/${id}`) 
-
     return {
         props: {
             postDetails: data
