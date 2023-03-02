d7636fe1a4088487d2fe9c436d6e305ca7f02781
kulklex <kulklex@gmail.com>
2023-03-02 15:31:26 +0100

    Added moe routes to post api

--- pages/api/posts/[id].tsx
+++ pages/api/posts/[id].tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { client } from "@/utils/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { postDetailQuery } from '../../../utils/queries';
 
-export default function Detail() {
-  return (
-    <div>Detail</div>
-  )
+
+
+const handler = async (req:NextApiRequest, res:NextApiResponse ) => {
+    if (req.method === 'GET') {
+        const {id} = req.query as {id: string}
+        const query = postDetailQuery(id)
+
+        const data = client.fetch(query)
+
+        res.status(200).json(data)
+    }
 }
+
+export default  handler
