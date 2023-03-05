d5dd2a6850016db261d70ba8d378b8db695f8d04
kulklex <kulklex@gmail.com>
2023-03-05 14:16:15 +0100

    Fetch all users func

--- store/authStore.ts
+++ store/authStore.ts
@@ -2,13 +2,19 @@ import {create} from 'zustand'
 import {persist} from 'zustand/middleware'
 import axios from 'axios'
 import { User } from '@/types'
-
+import { BASE_URL } from '@/utils'
 
 const authStore = (set:any) => ({
     user: null,
 
     addUser: (user: User) => set({user: user}),
-    removeUser: () => set({user: null})
+    removeUser: () => set({user: null}),
+
+    fetchAllUsers: async() => {
+        const {data} = await axios.get(`${BASE_URL}/api/users`)
+        
+        set({allUsers: data})
+    }
 })
 
 const useAuthStore = create(
