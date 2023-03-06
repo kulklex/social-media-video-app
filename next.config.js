c7331c104c9a07d34f5750747267b7426abae1f5
kulklex <kulklex@gmail.com>
2023-03-06 13:34:54 +0100

    updated nextConfig file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ['encrypted-tbn0.gstatic.com', 'lh3.googleusercontent.com'],
